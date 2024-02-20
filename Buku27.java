5f9d0f8deed18971961220dbf53f037d4785d988
siskanuriaprilia <aprilias878@gmail.com>
2024-02-21 01:31:08 +0700

    pmbaruan

--- Buku27.java
+++ Buku27.java
@@ -1,29 +1,28 @@
 public class Buku27 {
-        
-        String judul, pengarang;
-        int halaman, stok, harga;
-        int gantiHarga;
 
-        void tampilInformasi()
-        {
-            System.out.println("Judul: " + judul);
-            System.out.println("Pengarang: "  + pengarang);
-            System.out.println("Jumlah halaman : "  + halaman);
-            System.out.println("Sisa stok: " + stok);
-            System.out.println("Harga: " + harga);
-        }
+    String judul, pengarang;
+    int halaman, stok, harga;
+    int gantiHarga;
 
-        void  terjual (int jml)
-        {
-            stok -= jml;
-        }
-        void restock(int jml)
-        {
-            stok += jml;
-        }
-        void gantiHarga(int hrg)
-        {
-        
-            harga = hrg;
-        }
+    void tampilInformasi()
+    {
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: "  + pengarang);
+        System.out.println("Jumlah halaman : "  + halaman);
+        System.out.println("Sisa stok: " + stok);
+        System.out.println("Harga: " + harga);
     }
+
+    void  terjual (int jml)
+    {
+        stok -= jml;
+    }
+        void restock(int jml)
+    {
+        stok += jml;
+    }
+        void gantiHarga(int hrg)
+    {
+        harga = hrg;
+    }
+}
