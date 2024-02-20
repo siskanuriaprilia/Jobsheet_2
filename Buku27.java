5ff82c45f5ba8901e781cdf8904622dab4a7ca93
siskanuriaprilia <aprilias878@gmail.com>
2024-02-21 01:08:41 +0700

    jobsheet 2 praktikum2

---
+++ Buku27.java
@@ -0,0 +1,30 @@
+
+public class Buku27 {
+        
+        String judul, pengarang;
+        int halaman, stok, harga;
+        int gantiHarga;
+
+        void tampilInformasi()
+        {
+            System.out.println("Judul: " + judul);
+            System.out.println("Pengarang: "  + pengarang);
+            System.out.println("Jumlah halaman : "  + halaman);
+            System.out.println("Sisa stok: " + stok);
+            System.out.println("Harga: " + harga);
+        }
+
+        void  terjual (int jml)
+        {
+            stok -= jml;
+        }
+        void restock(int jml)
+        {
+            stok += jml;
+        }
+        void gantiHarga(int hrg)
+        {
+        
+            harga = hrg;
+        }
+    }
